39d52e119299e9b79f429cf35a2523e88688796d
israelb <israelbinunsky@gmail.com>
2025-05-22 12:31:13 +0300

    class hamas

---
+++ idf-operation/hamas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class hamas
+{
+    public List<terrorist> terrorists;
+
+
+    public static terrorist createTer(string name, int rank, string Weapon)
+    {
+        terrorist ter = new terrorist(name, rank, Weapon, true);
+        return ter;
+    }
+
+}
