8d7c326f58d7b6f826eb2a90529d1530681e1747
israelb <israelbinunsky@gmail.com>
2025-05-22 14:34:11 +0300

    terrorists hamas

--- idf-operation/hamas.cs
+++ idf-operation/hamas.cs
@@ -5,11 +5,21 @@ class hamas
 {
     public List<terrorist> terrorists;
 
-
-    public static terrorist createTer(string name, int rank, string Weapon)
+    public hamas()
     {
-        terrorist ter = new terrorist(name, rank, Weapon, true);
-        return ter;
+        terrorists = new List<terrorist>();
+        addTerrorist("muhamad", 5, "AK47");
+        addTerrorist("ali", 4, "M16");
+        addTerrorist("ahmad", 3, "M16");
+        addTerrorist("mustafa", 3, "gun");
+        addTerrorist("abed", 2, "knife");
+        addTerrorist("saud", 1, "knife");
+
     }
 
+    public void addTerrorist(string name, int rank, string Weapon)
+    {
+        terrorist ter = new terrorist(name, rank, Weapon, true);
+        this.terrorists.Add(ter);
+    }
 }
